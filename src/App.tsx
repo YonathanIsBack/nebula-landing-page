cd86cecbf25157b38f9f9332106c5dfbf0c9dc97
Yonathan Pandapotan <yonabaho@gmail.com>
2026-04-26 16:07:54 +0700

    Add About me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Menu, Row, Col, Typography } from "antd";
+import { Menu, Image, Row, Col, Typography } from "antd";
 import type { MenuProps } from "antd";
 import { useState } from "react";
 import "./App.css";
+import { Content, Footer, Header } from "antd/es/layout/layout";
 
 const { Text, Title } = Typography;
 type MenuItem = Required<MenuProps>["items"][number];
@@ -42,39 +43,73 @@ function App() {
   };
 
   return (
-    <Row
-      style={{
-        borderBottomWidth: "1px",
-        borderColor: "rgba(5, 5, 5, 0.06)",
-        borderBottomStyle: "solid",
-      }}
-    >
-      <Col
-        span={12}
+    <>
+      <Row
         style={{
-          alignItems: "center",
-          display: "flex",
-          justifyContent: "center",
+          borderBottomWidth: "1px",
+          borderColor: "rgba(5, 5, 5, 0.06)",
+          borderBottomStyle: "solid",
         }}
       >
-        <Title level={3} style={{ margin: "0" }}>
-          User Why Nebula
-        </Title>
-      </Col>
-      <Col span={12}>
-        <Menu
-          onClick={onClick}
-          selectedKeys={[current]}
-          mode="horizontal"
-          items={items}
+        <Col
+          span={8}
           style={{
-            justifyContent: "flex-end",
-            borderBottomWidth: "0px",
-            height: "100%",
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "center",
           }}
-        />
-      </Col>
-    </Row>
+        >
+          <Title level={3} style={{ margin: "0" }}>
+            User Why Nebula
+          </Title>
+        </Col>
+        <Col span={16}>
+          <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items}
+            style={{
+              justifyContent: "flex-end",
+              borderBottomWidth: "0px",
+              height: "100%",
+            }}
+          />
+        </Col>
+      </Row>
+      <Content>
+        <Row>
+          <Col
+            style={{
+              height: "90vh",
+              width: "100%",
+              backgroundColor: "rgba(0,0,0,0.25)",
+            }}
+          >
+            <Row style={{ height: "100%" }}>
+              <Col
+                span={24}
+                style={{
+                  textAlign: "left",
+                  paddingLeft: "42px",
+                  paddingRight: "42px",
+                  paddingTop: "20%",
+                }}
+              >
+                <Title>Good Morning!</Title>
+                <Text>
+                  My name is Yonathan and I am an IT Professional currently
+                  working as a Fullstack Engineer in Indonesia. Experienced and
+                  passionate on Website, API, Database, Mobile and Application
+                  Development.
+                </Text>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Content>
+      <Footer></Footer>
+    </>
   );
 }
 
