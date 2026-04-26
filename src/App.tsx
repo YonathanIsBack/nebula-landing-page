17bc7cf7d0062e1eb14ddfabf573929b096ac87c
Yonathan Pandapotan <yonabaho@gmail.com>
2026-04-26 15:43:09 +0700

    Create menubar

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Menu, Row, Col, Typography } from "antd";
+import type { MenuProps } from "antd";
+import { useState } from "react";
+import "./App.css";
+
+const { Text, Title } = Typography;
+type MenuItem = Required<MenuProps>["items"][number];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [current, setCurrent] = useState("mail");
+  const items: MenuItem[] = [
+    {
+      label: "About Me",
+      key: "about-me",
+      icon: "",
+    },
+    {
+      label: "Background",
+      key: "background",
+      icon: "",
+    },
+    {
+      label: "Work Done",
+      key: "work-done",
+      icon: "",
+    },
+    {
+      label: "Contact Person",
+      key: "contact-person",
+      icon: "",
+    },
+    {
+      label: "About Something Else",
+      key: "about-something-else",
+      icon: "",
+    },
+  ];
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <Row
+      style={{
+        borderBottomWidth: "1px",
+        borderColor: "rgba(5, 5, 5, 0.06)",
+        borderBottomStyle: "solid",
+      }}
+    >
+      <Col
+        span={12}
+        style={{
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Title level={3} style={{ margin: "0" }}>
+          User Why Nebula
+        </Title>
+      </Col>
+      <Col span={12}>
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+          style={{
+            justifyContent: "flex-end",
+            borderBottomWidth: "0px",
+            height: "100%",
+          }}
+        />
+      </Col>
+    </Row>
+  );
 }
 
-export default App
+export default App;
